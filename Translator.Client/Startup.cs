09c42be4c36d6c9451c44937e8b18f0d12f823bf
Herman Ramos <hermanramos@Hermans-MacBook.local>
2019-12-14 10:41:27 -0600

    removed comment

--- Translator.Client/Startup.cs
+++ Translator.Client/Startup.cs
@@ -38,7 +38,6 @@ namespace Translator.Client
             services.AddMvc(o => o.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
